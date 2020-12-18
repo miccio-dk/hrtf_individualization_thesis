171ec3cd109fdf2288d78710a38ececda09e792f
Riccardo Miccini <rimiccini@yahoo.it>
2020-12-18 15:17:23 +0100

    fixed interaural configs

--- calc_test_sd_fullsystem.py
+++ calc_test_sd_fullsystem.py
@@ -172,7 +172,7 @@ def main():
                 z_hrtf = LatentDataset.pca_inverse_transform(z_hrtf, scaler, pca)
                 z_hrtf = torch.tensor(z_hrtf).to(args.device)
             # z_hrtf to hrtf
-            c2 = c[:,0:1] if args.force_c else c
+            c2 = c[:, 0:1] if args.force_c else c
             resp_pred = models['hrtf'].cvae.dec(z_hrtf, c2)
         resp_true = matching_resp_true(hrtf_ds, hrtf_ds_labels, labels['subj'], labels['ear'], c)
         assert len(resp_true) == len(resp_pred)
